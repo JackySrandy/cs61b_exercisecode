c37a866d3707c5b812264032dddb85fff9b40b77
Jacky <snapvivalavida@gmail.com>
2024-08-03 15:28:01 +0800

    A triky commit

--- BST61B/src/MyBST/BST.java
+++ BST61B/src/MyBST/BST.java
@@ -1,22 +1,93 @@
 package MyBST;
 
-class BST<Key> {
-    private Key key;
-    private BST left;
-    private BST right;
+import java.util.Comparator;
 
-    BST(Key key) {
-        this.key = key;
+class BST<T extends Comparable<T>> {
+    private T key;
+    private BST<T> left;
+    private BST<T> right;
+
+    BST(T k){
+        key = k;
+        left = right = null;
+    }
+    BST(T k, BST<T> l, BST<T> r) {
+        key = k;
+        left = l;
+        right = r;
     }
 
-    BST(Key key, BST<Key> left, BST<Key> right) {
-        this.key = key;
-        this.left = left;
-        this.right = right;
+    static <T extends Comparable<T>> BST<T> insert(BST<T> t, T k) {
+        BST<T> leaf = new BST<>(k);
+        return BST.insert(t, leaf);
+    }
+    static <T extends Comparable<T>> BST<T> insert(BST<T> t, BST<T> k) {
+        if (t == null) {
+            return k;
+        }
+        if (k.key.compareTo(t.key) > 0) {
+            t.right = insert(t.right, k);
+        } else {
+            t.left = insert(t.left, k);
+        }
+        return t;
     }
 
-    static BST find(BST tree, <Key> sk){
-        return null;
+    static <T extends Comparable<T>> BST<T> delete(BST<T> t, T k) {
+        BST<T> ik = new BST<>(k);
+        return delete(t, ik);
     }
 
+    static <T extends Comparable<T>> BST<T> delete(BST<T> t, BST<T> k) {
+        if (t == null) {
+            return t;
+        }
+        if (k.key.compareTo(t.key) > 0) {
+            t.right = delete(t.right, k);
+        } else if (k.key.compareTo(t.key) < 0) {
+            t.left = delete(t.left, k);
+        } else {
+            if (t.left == null) {
+                return t.right;
+            } else if (t.right == null) {
+                return t.left;
+            }
+            T newKey = minKey(t.right);
+            t.key = newKey;
+            t.right = delete(t.right, newKey);
+        }
+        return t;
+    }
+
+    private static <T extends Comparable<T>> T minKey(BST<T> t) {
+        if (t == null) {
+            return null;
+        }
+        while(t.left != null) {
+            t = t.left;
+        }
+        return t.key;
+    }
+
+    static <T extends Comparable<T>> T find(BST<T> t, T k) {
+        if(t == null) {
+            return null;
+        }
+        if (t.key.compareTo(k) > 0) {
+            return find(t.left, k);
+        } else if (t.key.compareTo(k) < 0) {
+            return find(t.right, k);
+        } else {
+            return k;
+        }
+    }
+
+    public static void main(String[] args) {
+        BST<Integer> r = new BST<>(7);
+        insert(r, 8);
+        insert(r, 6);
+        insert(r, 5);
+        insert(r, 9);
+        delete(r, 7);
+    }
 }
